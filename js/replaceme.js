0775b6d88b518f1acda9e37ea80a9caad651bdd1
Isaac Zerououl <P2557506@my365.dmu.ac.uk>
2022-03-17 14:02:16 +0000

    New revision

--- js/replaceme.js
+++ js/replaceme.js
@@ -23,13 +23,15 @@ btnScrollToTop.addEventListener('click', function() {
   });
 });
 
-document.addEventListener('click', ev => {
-  const isDropdownButton = ev.target.matches("[data-dropdown-button]")
-  if (!isDropdownButton && ev.target.closest('[data-dropdown]') != null) return
+//JS dropdown
+
+document.addEventListener('click', e => {
+  const isDropdownButton = e.target.matches("[data-dropdown-button]")
+  if (!isDropdownButton && e.target.closest('[data-dropdown]') != null) return
 
 let currentDropdown
   if(isDropdownButton){
-    currentDropdown = ev.target.closest('[data-dropdown]')
+    currentDropdown = e.target.closest('[data-dropdown]')
     currentDropdown.classList.toggle('active')
   }
 
@@ -38,12 +40,12 @@ let currentDropdown
     if(dropdown === currentDropdown) return
     dropdown.classList.remove('active')
 
-  });
+  })
 
 })
 
 for (const element of document.querySelectorAll('nav a')) {
-	element.addEventListener('click', ev => {
+	element.addEventListener('click', e => {
 		menuToggler.classList.remove('open');
 	});
 }
